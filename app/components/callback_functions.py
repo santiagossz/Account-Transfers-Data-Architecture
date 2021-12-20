f1c41bd8eb93132580d607d2fe8db41cbcd46835
santiagossz <dr.santiagossz@gmail.com>
2021-12-20 15:47:38 -0500

    Account Monthly Balance report

--- app/components/callback_functions.py
+++ app/components/callback_functions.py
@@ -32,4 +32,14 @@ def show_upload(pathname):
             'margin': '10px',
         }
     else:
-        return {'display': 'none'}
+        return {
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px',
+        }
+        # return {'display': 'none'}
--- app/components/layouts.py
+++ app/components/layouts.py
@@ -17,6 +17,8 @@ def set_layout():
     html.Br(),
     dcc.Link('Account Monthly Balance', href='/amb'),
     html.Br(),
+    dcc.Link('PIX Metrics', href='/aa'),
+    html.Br(),
     html.Div(id='display-page')
 
 ])
@@ -27,8 +29,13 @@ def index_layout(filename):
                 ])
 
 def amb_layout(df):
+    # df['month_number']=df['month_number'].astype(int)
+
     return html.Div([dash_table.DataTable(
             data=df.to_dict('records'),
-            columns=[{'name': i, 'id': i} for i in df.columns]
+            columns=[{'name': i, 'id': i, 'type':'text' if i=='full_name' else 'numeric'} for i in df.columns],
+            filter_action='native',
+            sort_action='native'
+            
         )])
     
